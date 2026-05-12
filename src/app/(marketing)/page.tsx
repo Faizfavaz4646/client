facedc5121038ef9975e3430c487d537450502fd
Faiz Favaz <faizfavaz7@gmail.com>
2026-05-12 12:36:07 +0530

    perf: move all heavy client components to dynamic imports to eliminate TBT

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 import Navbar from './Navbar';
 import HeroSection from './HeroSection';
-import MarketingSections from './MarketingSections';
+const MarketingSections = dynamic(() => import('./MarketingSections'), { ssr: false });
 
 export default function MarketingPage() {
   return (
